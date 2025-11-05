ffc6d7a5cf4fa411eb77bed8a1f30485a93b4d38
caio <sedeicaio@gmail.com>
2025-11-04 21:49:25 -0300

    review-delete-alterar-pegarUm

--- src/controller/produto.js
+++ src/controller/produto.js
@@ -2,7 +2,7 @@ import ServiceProduto from "../service/produto.js"
 
 class ControllerProduto {
 
-    async PegarTodos (req, res) {
+    async PegarTodos(req, res) {
         try {
             const produtos = await ServiceProduto.PegarTodos()
             res.status(200).send({
@@ -13,15 +13,20 @@ class ControllerProduto {
         }
     }
 
-    async PegarUm (req, res) {
+    async PegarUm(req, res) {
         try {
-            res.status(200).send('oi')
+            // const { id } = req.params
+            const id = req.params.id
+            const produto = await ServiceProduto.PegarUm(id)
+            res.status(200).send({
+                data: produto
+            })
         } catch (error) {
             res.status(500).send({ mgs: error.message })
         }
     }
 
-    async Criar (req, res) {
+    async Criar(req, res) {
         try {
             const { nome, disponivel, qtde } = req.body
             await ServiceProduto.Criar(nome, disponivel, qtde)
@@ -31,17 +36,28 @@ class ControllerProduto {
         }
     }
 
-    async Alterar (req, res) {
+    async Alterar(req, res) {
         try {
-            res.status(200).send('oi')
+            // const { nome, disponivel, qtde } = req.body
+            const id = req.params.id
+            const nome = req.body?.nome
+            const disponivel = req.body?.disponivel
+            const qtde = req.body?.qtde
+            await ServiceProduto.Alterar(id, nome, disponivel, qtde)
+            
+            res.status(200).send({
+                data: 'produto alterado com sucesso'
+            })
         } catch (error) {
             res.status(500).send({ mgs: error.message })
         }
     }
 
-    async Deletar (req, res) {
+    async Deletar(req, res) {
         try {
-            res.status(200).send('oi')
+            const id = req.params.id
+            await ServiceProduto.Deletar(id)
+            res.status(204).send()
         } catch (error) {
             res.status(500).send({ mgs: error.message })
         }
--- src/service/produto.js
+++ src/service/produto.js
@@ -6,7 +6,18 @@ class ServiceProdutos {
         return Produto.findAll()
     }
 
-    async PegarUm(id) {}
+    async PegarUm(id) {
+        if (!id) {
+            throw new Error('Favor informar ID.')
+        }
+        const produto = await Produto.findByPk(id)
+
+        if (!produto) {
+            throw new Error('Produto não encontrado.')
+        }
+
+        return produto
+    }
 
     async Criar(nome, disponivel, qtde) {
         await Produto.create({
@@ -14,9 +25,35 @@ class ServiceProdutos {
         })
     }
 
-    async Alterar(id, nome, disponivel, qtde) {}
+    async Alterar(id, nome, disponivel, qtde) {
+        if (!id) {
+            throw new Error('Favor preencher todas as info.')
+        }
+        const produtovelho = await Produto.findByPk(id)
+        if (!produtovelho) {
+            throw new Error('Produto não encontrado.')
+        }
 
-    async Deletar(id) {}
+        produtovelho.nome = nome || produtovelho.nome
+        produtovelho.disponivel = disponivel || produtovelho.disponivel
+        produtovelho.qtde = qtde || produtovelho.qtde
+
+        produtovelho.save
+    }
+
+
+    async Deletar(id) {
+        if (!id) {
+            throw new Error('Favor informar ID.')
+        }
+        const produto = await Produto.findByPk(id)
+
+        if (!produto) {
+            throw new Error('Produto não encontrado.')
+        }
+
+        await produto.destroy()
+    }
 
 }
 
